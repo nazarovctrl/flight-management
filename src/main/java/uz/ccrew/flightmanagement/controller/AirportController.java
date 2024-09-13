08a4d44056c5d5a9583260b250d092b41e5286b4
maksud9909 <rmaksud822@gmail.com>
2024-09-13 13:39:23 +0500

    add Airport admin

--- src/main/java/uz/ccrew/flightmanagement/controller/AirportController.java
+++ src/main/java/uz/ccrew/flightmanagement/controller/AirportController.java
@@ -1,5 +1,7 @@
 package uz.ccrew.flightmanagement.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import uz.ccrew.flightmanagement.dto.Response;
 import uz.ccrew.flightmanagement.dto.ResponseMaker;
 import uz.ccrew.flightmanagement.dto.airport.AirportCreateDTO;
@@ -19,12 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/airport")
 @RequiredArgsConstructor
 @Tag(name = "Airport Controller", description = "Airport API")
+@SecurityRequirement(name = "Bearer Authentication")
 public class AirportController {
     private final AirportService airportService;
 
     @PostMapping("/add")
     @Operation(summary = "Add airport, role admin")
-    public ResponseEntity<Response<AirportDTO>> addAirport(@RequestBody AirportCreateDTO airportCreateDTO) {
+    public ResponseEntity<Response<AirportDTO>> addAirport(@Valid @RequestBody AirportCreateDTO airportCreateDTO) {
         AirportDTO result = airportService.addAirport(airportCreateDTO);
         return ResponseMaker.ok(result);
     }
