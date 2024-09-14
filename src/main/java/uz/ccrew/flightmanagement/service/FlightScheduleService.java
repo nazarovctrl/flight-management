eae4af05ffc1992339c98d3164d11eb5326a18f6
maksud9909 <rmaksud822@gmail.com>
2024-09-14 21:31:55 +0500

    departureDateTime

--- src/main/java/uz/ccrew/flightmanagement/service/FlightScheduleService.java
+++ src/main/java/uz/ccrew/flightmanagement/service/FlightScheduleService.java
@@ -4,8 +4,6 @@ import org.springframework.data.domain.Page;
 import uz.ccrew.flightmanagement.dto.flightSchedule.FlightScheduleCreateDTO;
 import uz.ccrew.flightmanagement.dto.flightSchedule.FlightScheduleDTO;
 
-import java.util.List;
-
 public interface FlightScheduleService {
     FlightScheduleDTO addFlightSchedule(FlightScheduleCreateDTO flightScheduleCreateDTO);
 
