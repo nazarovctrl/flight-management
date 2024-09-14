72e9f7bb06610e72576f22e62b4bda1bda375bb9
maksud9909 <rmaksud822@gmail.com>
2024-09-14 12:03:26 +0500

    UpdateLeg

--- src/main/java/uz/ccrew/flightmanagement/dto/leg/LegUpdateDTO.java
+++ src/main/java/uz/ccrew/flightmanagement/dto/leg/LegUpdateDTO.java
@@ -10,5 +10,3 @@ public record LegUpdateDTO(@NotNull(message = "actual departure time can not be 
                            LocalDateTime actualDepartureTime,
                            @NotNull(message = "actual arrival time can not be null")
                            LocalDateTime actualArrivalTime) {}
-
-
