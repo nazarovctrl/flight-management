d22780bcd1d88c9ca1cc39a23d7b43aac618c867
maksud9909 <rmaksud822@gmail.com>
2024-09-16 11:39:48 +0500

    Ready

--- src/main/java/uz/ccrew/flightmanagement/repository/PassengerRepository.java
+++ src/main/java/uz/ccrew/flightmanagement/repository/PassengerRepository.java
@@ -2,12 +2,10 @@ package uz.ccrew.flightmanagement.repository;
 
 import uz.ccrew.flightmanagement.entity.Passenger;
 
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 public interface PassengerRepository extends BasicRepository<Passenger, Long> {
     boolean existsByCustomerId(Long customerId);
+
     Optional<Passenger> findByCustomer_Id(Long customerId);
 }
